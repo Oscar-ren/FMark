efa5e899a0fefd08c3698bd47c828e2bacb35b81
renxianglei <renxianglei@renxiangleideMacBook-Pro.local>
2016-11-08 19:06:23 +0800

    fix

--- www/static/js/index.js
+++ www/static/js/index.js
@@ -16,9 +16,7 @@ class FMark {
         this.fmarkList = [];
         //标识,是否是mouseup事件后的点击事件
         this.isMouseUp = false;
-
         this.markPopup = undefined;
-
         this.host = 'http://localhost:8360';
     }
 
@@ -74,22 +72,6 @@ class FMark {
             }
         }
 
-
-        /**
-         * 弹窗事件
-         */
-        $(document).on('click', function(e) {
-
-            if(_this.isMouseUp) {
-                _this.isMouseUp = false;
-                return;
-            }
-            if($('.action-list')[0] && $.contains($('.action-list')[0],e.target) || $('.action-list')[0] == e.target) {
-                return;
-            }
-            $('.action-list').hide();
-        })
-
         /**
          * 保证只有拖拽下选择的文本才会输出
          */
@@ -112,15 +94,13 @@ class FMark {
                 //选中区域有文字
                 if(selObj.toString()) {
 
+                    //需要储存的信息
                     let currentRangeInfo = {
-                        startContainer: selRange.startContainer,
-                        startOffset:  selRange.startOffset,
-                        textLength:  $.trim(selRange.toString()).length,
-                        commonTag: selRange.commonAncestorContainer.nodeName,
-                        tagIndex: $(selRange.commonAncestorContainer).index(selRange.commonAncestorContainer.nodeName)
+                        start_index: traversalStartLen(selRange),
+                        text_length:  $.trim(selRange.toString()).length,
+                        common_tag: selRange.commonAncestorContainer.nodeName,
+                        tag_index: $(selRange.commonAncestorContainer).index(selRange.commonAncestorContainer.nodeName)
                     }
-
-                    console.log(traversalStartLen(selRange));
 
                     //起止文本在一个元素内
                     if(selRange.startContainer == selRange.endContainer) {
@@ -130,15 +110,10 @@ class FMark {
                         //选中的文本是跨元素的,所以父级元素肯定有孩子元素
                         _this.transfer(currentRangeInfo);
                     }
-                    // _this.fmarkList.push(currentRangeInfo);
 
                     //TODO 存本地调试
+                    // _this.fmarkList.push(currentRangeInfo);
                     // localStorage.setItem('fmark', JSON.stringify(_this.fmarkList));
-
-                    //TODO 挂载弹窗,最好是promise,需要位置绝对定位,e.clientX,e.clientY
-                    _this.isMouseUp = true;
-                    //$('.action-list').show().css('top', e.clientY + 'px').css('left', e.clientX + 'px');
-
                 }
             }
             $(document).off('mousemove');
@@ -148,22 +123,21 @@ class FMark {
 
     /**
      * 将选取文本转换成特定样式
-     * 问题: 长度少一,原因是找到了开始节点后添加了子节点,又遍历了子节点,导致子节点被减两次
-     * 解决方法: 设置个flag,开始节点后跳过其子节点
      * @param info range对象信息
      * @returns {number}
      */
     transfer(info) {
 
-        let startOffset = info.startOffset,
-            startContainer = info.startContainer,
+        let startIndex = info.start_index,
             //剩余长度
-            restTextLen = info.textLength,
+            restTextLen = info.text_length,
             //第一次是公共父节点
-            ancestorNode = $(info.commonTag)[info.tagIndex],
+            ancestorNode = $(info.common_tag)[info.tag_index],
             //起止游标
             startSearched = false,
-            endSearched = false;
+            endSearched = false,
+            //已查找长度
+            beginTextLen = 0;
 
         /**
          * 遍历节点渲染
@@ -172,19 +146,24 @@ class FMark {
 
             if (endSearched) return 2;
 
-            if (currentNode == startContainer) {
-                //深度遍历替换元素
-                startSearched = true;
-                let nextSibling = currentNode.splitText(startOffset);
-
-                //判断长度,初始节点不可能和结束节点在同一个dom上
-                restTextLen -= $.trim(nextSibling.nodeValue).length;
-                changeNodeStyle(currentNode.nextSibling);
-                //找到了开头,下一个循环
-                return 1;
+            if(!startSearched && currentNode.nodeType == 3) {
+                let normalTextLen = currentNode.nodeValue.length;
+                if(beginTextLen + normalTextLen >= startIndex) {
+                    startSearched = true;
+                    currentNode.splitText(startIndex - beginTextLen);
+                    console.log('startSearched', currentNode.nextSibling);
+                    //判断长度,初始节点不可能和结束节点在同一个dom上
+                    changeNodeStyle(currentNode.nextSibling);
+                    //找到了开头,下一个循环
+                    return 1;
+                }else {
+                    //没找到开始节点,接着找
+                    beginTextLen += normalTextLen;
+                    console.log(currentNode, beginTextLen);
+                }
             }
             //已经找到开始节点,且当前节点是text节点,且不为空
-            if (startSearched && currentNode.nodeType == 3 && currentNode.nodeValue.length > 0) {
+            if (startSearched && currentNode.nodeType == 3) {
                 let normalTextLen = currentNode.nodeValue.length;
 
                 //判断长度
@@ -196,26 +175,18 @@ class FMark {
                     return 2;
                 } else {
                     restTextLen -= normalTextLen;
+                    console.log(currentNode);
                     changeNodeStyle(currentNode);
                 }
             }
 
-            let nodes = currentNode.childNodes,
-                flag = false;
+            let nodes = currentNode.childNodes;
 
             //遍历孩子节点
             for (let i = 0; i < nodes.length; i++) {
                 let node = nodes[i];
-                //flag阻止开始节点子节点的第二次计算
-                if (flag) {
-                    flag = false;
-                    continue;
-                }
                 let result = traversalRender(node);
                 if (result == 2) break;
-                if (result == 1) {
-                    flag = true;
-                }
             }
         }
 
